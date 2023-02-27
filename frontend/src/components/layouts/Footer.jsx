0642b61c816f4db5cc337a3c99612a96b29f565c
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-02-27 17:20:34 -0300

    [fix] ancor

--- frontend/src/components/layouts/Footer.jsx
+++ frontend/src/components/layouts/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
   return (
     <footer className='footer'>
       <p>
-        Desenvolvido por<span className='bold'> <Link to='https://www.linkedin.com/in/anderson-costa-rodrigues/'>Anderson Rodrigues</Link></span>
+        Desenvolvido por<span className='bold'> <a href="https://www.linkedin.com/in/anderson-costa-rodrigues/" target='_blank' rel="noreferrer">Anderson Rodrigues</a></span>
         <span className='bold'>{' - '}Get a Pet</span> &copy; 2023
       </p>
     </footer>
