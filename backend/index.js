e5975c6dae10913c24cc72a8e3511a8ae22e53e3
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-02-27 17:52:44 -0300

    [feat] try default config cors

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,14 @@ app.use(express.json());
 app.use(cookieParser());
 
 // Solve CORS
-app.use(cors());
+app.use(cors(
+  {
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204,
+  }
+));
 
 // Routes
 app.use('/users', userRoutes);
