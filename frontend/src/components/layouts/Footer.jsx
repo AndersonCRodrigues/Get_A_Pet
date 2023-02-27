d971117145ed3d9f900cd7438c7fada470c5d796
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-02-27 17:08:29 -0300

    [feat] linkedin link in footer

--- frontend/src/components/layouts/Footer.jsx
+++ frontend/src/components/layouts/Footer.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Footer.css';
+import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
     <footer className='footer'>
       <p>
-        <span className='bold'>Get a Pet</span> &copy; 2023
+        Desenvolvido por<span className='bold'> <Link to='https://www.linkedin.com/in/anderson-costa-rodrigues/'>Anderson Rodrigues</Link></span>
+        <span className='bold'>{' - '}Get a Pet</span> &copy; 2023
       </p>
     </footer>
   )
